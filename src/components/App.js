a37312f134afbc163576600f63c1516833b7e638
emmafewer <emmafewer20@gmail.com>
2021-02-09 07:29:53 -0800

    done

--- src/components/App.js
+++ src/components/App.js
@@ -21,10 +21,32 @@ class App extends Component {
     this.setState({selectedHog: hog})
   }
 
+  handleSort = (e) => {
+    switch (e.target.value) {
+      case 'greased':
+        let filteredHogs = hogs.filter(hog => hog.greased == true)
+        this.setState({hogs:filteredHogs})
+      case 'name':
+        let sortedNames = hogs.sort((a,b) => (a.name > b.name)? 1 : -1)
+        this.setState({hogs:sortedNames})
+      case 'weight':
+        let sortedWeights = hogs.sort((a,b) => (a.weight > b.weight))
+        this.setState({hogs:sortedWeights})
+      default:
+        this.setState({hogs:hogs})
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <Nav />
+        <select onChange={this.handleSort}>
+            <option value="default">Sort Hog</option>
+            <option value="greased">Greased</option>
+            <option value="name">Name</option>
+            <option value="weight">weight</option>
+        </select>
         {this.state.selectedHog ? <MainHog selectedHog={this.state.selectedHog} formatImage={this.formatImage}/> : null}
         <HogContainer hogs={this.state.hogs} formatImage={this.formatImage} showDetails={this.showDetails}/>
       </div>
